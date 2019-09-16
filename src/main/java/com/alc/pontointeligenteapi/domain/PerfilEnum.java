aa8445b18d010cb96475ebbcf4bbd7790d63178f
Anderson Lemos Camelo <andersonlemos83@gmail.com>
2019-09-16 15:57:26 -0300

    Commit com criação dos repositórios e seus testes

---
+++ src/main/java/com/alc/pontointeligenteapi/domain/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.alc.pontointeligenteapi.domain;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
---
+++ src/main/java/com/alc/pontointeligenteapi/repository/EmpresaRepository.java
@@ -0,0 +1,12 @@
+package com.alc.pontointeligenteapi.repository;
+
+import com.alc.pontointeligenteapi.domain.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+
+    @Transactional(readOnly = true)
+    Empresa findByCnpj(String cnpj);
+
+}
